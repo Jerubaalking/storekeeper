895738f14e65f5300ac4dc943a26839e8dc6fe4f
Gideon Sainyeye <gsainyeye@gmail.com>
2023-04-26 16:22:05 +0300

    captain-definition commit 5

--- database/mysql.js
+++ database/mysql.js
@@ -1,16 +1,21 @@
 const dbConfig = require("../database/config");
 const mysql = require('mysql2/promise');
 const { Sequelize, DataTypes, Model } = require("sequelize");
+var sequelize;
+const initialize = async () => {
+  const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT } = dbConfig;
+  const connection = await mysql.createConnection({ DB_HOST, DB_PORT, DB_USER, DB_PASSWORD });
+  connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbConfig}\`;`);
+  sequelize = await new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, { dialect: dbConfig.dialect });
+  return sequelize;
 
-const { DB_NAME, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT } = dbConfig;
-const connection = await mysql.createConnection({ DB_HOST, DB_PORT, DB_USER, DB_PASSWORD });
-await connection.query(`CREATE DATABASE IF NOT EXISTS \`${dbConfig}\`;`);
+}
+
 
 // connect to db
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, { dialect: dbConfig.dialect });
 
 // init models and add them to the exported db object
-
+initialize().then(ans => console.log(ans)).catch(err => console.log(err));
 const Op = Sequelize.Op;
 
 module.exports = { Sequelize, sequelize, DataTypes, Op, Model };
