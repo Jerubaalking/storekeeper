28f261098d1b0c16ed8959909baa6afd41011482
Gideon Sainyeye <gsainyeye@gmail.com>
2023-04-26 14:17:38 +0300

    captain-definition commit 5

--- database/config.js
+++ database/config.js
@@ -1,14 +1,24 @@
 module.exports = {
+
+    "DB_HOST": "127.0.0.1",
+
+    "DB_USER": "root",
+
+    "DB_PASSWORD": "thina2023",
+
+    "DB": "storekeeper247_db",
+
     "dialect": "mysql",
-    "pool": {
 
-        "max": 5,
+    pool: {
 
-        "min": 0,
+        max: 5,
 
-        "acquire": 30000,
+        min: 2,
 
-        "idle": 10000
+        acquire: 30000,
+
+        idle: 10000
 
     }
 
