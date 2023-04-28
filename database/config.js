c04a6bedc969df73a20cb25a17891e1a262ff00d
Gideon Sainyeye <gsainyeye@gmail.com>
2023-04-28 19:42:03 +0300

    changes to db config

--- database/config.js
+++ database/config.js
@@ -1,10 +1,10 @@
 module.exports = {
     "DB_HOST": "127.0.0.1",
     "DB_USER": "root",
-    "DB_PASSWORD": "thina2023",
+    "DB_PASSWORD": "thina@2023",
     "DB_HOST2":"172.17.0.1",
     "DB_NAME": "storekeeper247_db",
-    "DB_PORT": 3307,
+    "DB_PORT": 3306,
     "dialect": "mysql",
     pool: {
 
