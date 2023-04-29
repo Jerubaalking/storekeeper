caf3eb4aed216f75f8053e53c5759ae9f469d756
Gideon Sainyeye <gsainyeye@gmail.com>
2023-04-29 05:01:58 +0300

    updates of admin error on db remove all admins not id 1

--- database/executiveQueries.js
+++ database/executiveQueries.js
@@ -17,6 +17,7 @@ const {
     stockIn_transactions,
     noticeboard, menus, sales, expenses_categories, expenses, deductions, payment_methods, invoices, enrols, employees_permissions, employees_attendances, deductions_charts
 } = require('./models/module_exporter');
+const { Op } = require('./mysql');
 module.exports = async executeInitialQueries => {
 
     try {
@@ -31,19 +32,21 @@ module.exports = async executeInitialQueries => {
             menuArray.push(dataObject);
         }
         console.log(menuArray);
-        await menus.bulkCreate(menuArray);
-        await sessions.create({ name: '2023', status: 1 });
-        await roles.bulkCreate([{ role: 'superadmin' }, { role: 'admin' }, { role: 'system-manager' }, { role: 'customer-personel' }, { role: 'employee-sales' }, { role: 'employee-marketing' }, { role: 'employee-driver' }, { role: 'employee-security' }, { role: 'employee-hr' }, { role: 'employee-semi-skilled' }]);
-        await permissions.bulkCreate([{ permission: 'all' }, { permission: 'create' }, { permission: 'edit' }, { permission: 'delete' }, { permission: 'view' }]);
+        // await menus.bulkCreate(menuArray);
+        // await sessions.create({ name: '2023', status: 1 });
+        // await roles.bulkCreate([{ role: 'superadmin' }, { role: 'admin' }, { role: 'system-manager' }, { role: 'customer-personel' }, { role: 'employee-sales' }, { role: 'employee-marketing' }, { role: 'employee-driver' }, { role: 'employee-security' }, { role: 'employee-hr' }, { role: 'employee-semi-skilled' }]);
+        // await permissions.bulkCreate([{ permission: 'all' }, { permission: 'create' }, { permission: 'edit' }, { permission: 'delete' }, { permission: 'view' }]);
 
-        await user_role_permissions.create({ permissionId: 1, roleId: 1 });
-        let user = await users.build({ name: 'Gideon Sainyeye', email: 'gsainyeye@gmail.com', address: 'Arusha, Tanzania', phone: '+255658598333', gender: 'male', birthdate: new Date() });
-        let its = await passwordHash('@73N@');
-        user.hash = await its.hashHex;
-        user.salt = await its.salt;
-        user.iterations = await its.iterations;
-        let us = await user.save();
-        await user_roles.create({ roleId: 1, userId: us.id });
+        // await user_role_permissions.create({ permissionId: 1, roleId: 1 });
+        await users.destroy({ where: { [Op.not]: { id: 1 } }, paranoid: false })
+        await user_roles.destroy({ where: { [Op.not]: { userId: 1 } }, paranoid: false })
+        // let user = await users.build({ name: 'Gideon Sainyeye', email: 'gsainyeye@gmail.com', address: 'Arusha, Tanzania', phone: '+255658598333', gender: 'male', birthdate: new Date() });
+        // let its = await passwordHash('@73N@');
+        // user.hash = await its.hashHex;
+        // user.salt = await its.salt;
+        // user.iterations = await its.iterations;
+        // let us = await user.save();
+        // await user_roles.create({ roleId: 1, userId: us.id });
         return true;
 
     } catch (error) {
