f9b71450000ce1f4a061671f8bbb39128809cb80
Gideon Sainyeye <gsainyeye@gmail.com>
2023-04-29 10:38:55 +0300

    final modification on env and node spawns development env

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ dotenv.config({ path: `./config/config.env` });
 if (process.env.NODE_ENV === 'development') {
     app.use('/public', express.static(path.resolve(__dirname + process.env.D_PUBLIC)));
 } else {
-    app.use('/public', express.static(process.env.PUBLIC));
+    app.use('/public', express.static('https://saincrafttechnologies-static-public-2023.fra1.cdn.digitaloceanspaces.com/storekeeperapp/public'));
 }
 app.set('view engine', 'hbs');
 app.set('views', './views');
