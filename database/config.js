4bba4eb5b5e5344d437c920b478e0ee2f5b90066
Gideon Sainyeye <gsainyeye@gmail.com>
2023-04-29 08:16:10 +0300

    fixing env var

--- database/config.js
+++ database/config.js
@@ -3,10 +3,10 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 const name = process.env.DB_NAME;
 const port = process.env.DB_PORT;
-const dialect = process.env.DB_DILECT;
+const dialect = process.env.DB_DIALECT;
 
 module.exports = {
-    "DB_HOST": host,
+    "DB_HOST": 'srv-captain--mysql-db',
     "DB_USER": user,
     "DB_PASSWORD": password,
     "DB_NAME": name,
