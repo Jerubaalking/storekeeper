98e31537a4a337903288100901b53710ac0a2883
Gideon Sainyeye <gsainyeye@gmail.com>
2023-05-06 12:10:47 +0300

    session and database modifications

--- database/config.js
+++ database/config.js
@@ -2,12 +2,12 @@
 
 module.exports = () => {
     return {
-        "DB_HOST": process.env.D_DB_HOST || '127.0.0.1',
-        "DB_USER": process.env.D_DB_USER || 'root',
-        "DB_PASSWORD": process.env.D_DB_PASSWORD || 'thina2023',
-        "DB_NAME": process.env.D_DB_NAME || 'storekeeper247_db',
-        "DB_PORT": process.env.D_DB_PORT || 3307,
-        "dialect": process.env.D_DB_DIALECT || 'mysql',
+        "DB_HOST": process.env.D_DB_HOST,
+        "DB_USER": process.env.D_DB_USER,
+        "DB_PASSWORD": process.env.D_DB_PASSWORD,
+        "DB_NAME": process.env.D_DB_NAME,
+        "DB_PORT": process.env.D_DB_PORT ,
+        "dialect": process.env.D_DB_DIALECT,
         pool: {
 
             max: 5,
