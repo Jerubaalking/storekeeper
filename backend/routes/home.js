2c90afc91f3827ff278ee292edafefa252a594ab
Gideon Sainyeye <gsainyeye@gmail.com>
2023-05-15 14:17:24 +0300

    updated landing

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -7,23 +7,23 @@ const { isLoggedIn } = require('../../passport/passport');
 const { menus, roles, stockOuts, items, customers, transactions } = require('../../database/models/module_exporter');
 // const upload = require('../controllers/services/multerConfig');
 
-router.get('/', isLoggedIn, async (req, res) => {
-    const control = await new Controllers(req);
-    let main = JSON.parse(JSON.stringify(await (await control.findBy()).main_menus({ where: {} })));
-    let sub = JSON.parse(JSON.stringify(await (await control.find()).sub_menus()));
-    // let expenses = JSON.parse(JSON.stringify(await (await control.find()).expenses()));
-    // let employees = await (await control.findBy()).employee();
+router.get('/', async (req, res) => {
+    // const control = await new Controllers(req);
+    // let main = JSON.parse(JSON.stringify(await (await control.findBy()).main_menus({ where: {} })));
+    // let sub = JSON.parse(JSON.stringify(await (await control.find()).sub_menus()));
+    // // let expenses = JSON.parse(JSON.stringify(await (await control.find()).expenses()));
+    // // let employees = await (await control.findBy()).employee();
 
-    let customer_personel = await (await control.findBy()).user({ where: {}, include: { model: roles } });
-    let customer_personels = [];
-    for (const personel of customer_personel.data) {
-        for (const role of personel.roles) {
-            if (role.role == 'customer-personel') {
-                customer_personels.push(personel);
-            }
-        }
+    // let customer_personel = await (await control.findBy()).user({ where: {}, include: { model: roles } });
+    // let customer_personels = [];
+    // for (const personel of customer_personel.data) {
+    //     for (const role of personel.roles) {
+    //         if (role.role == 'customer-personel') {
+    //             customer_personels.push(personel);
+    //         }
+    //     }
 
-    }
+    // }
     /** Invoice */
     // let invoicess = await (await control.findBy()).invoice({
     //     where: {}, include: [{ model: customers }, { model: stockOuts, include: [{ model: items }] }, { model: transactions }]
@@ -53,13 +53,15 @@ router.get('/', isLoggedIn, async (req, res) => {
     // let customerss = await (await control.find()).customers();
     // let staff = await (await control.find()).staff();
     // let expenses = JSON.parse(JSON.stringify(await (await control.find()).expenses()));
-    let payload = await (await control.getCurrentSession());
-    console.log('customer-personels', customer_personels);
-    res.render('home', {
-        viewManager: payload,
-        menus: main, sub_menu: sub
-    });
+    // let payload = await (await control.getCurrentSession());
+    // console.log('customer-personels', customer_personels);
+    // res.render('home', {
+    //     viewManager: payload,
+    //     menus: main, sub_menu: sub
+    // });
+    res.render('index', { layout: 'landing' });
 });
+
 router.get('/home', isLoggedIn, async (req, res) => {
 
     const control = await new Controllers(req);
